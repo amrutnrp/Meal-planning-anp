b2aab599da8d880c893d55ff642df54f3c3ab660
amrutnrp <60325062+amrutnrp@users.noreply.github.com>
2024-09-07 23:15:30 +0530

    basic skeleton done

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,41 @@
 import streamlit as st
 
+
+st.set_page_config(layout="wide")
+col1, col2, col3 = st.columns([1,1, 2])
+
+try:
+    from yaml_digest import meal_info
+except:
+    print('LOL')
+    " Error encountered in meal processor "
+    st.stop()
+
+def show_reciepe(arg ):
+    st.session_state["txtarea"] = str(meal_info[arg])
+    
+
+
+
+
+meal_names = [ i['dish'] for i in meal_info]
+with col1:
+    " # Browse meals"
+    st.text_input(label = "filter", value  = "",  key = "search" )
+    
+    flt_text = st.session_state["search"]
+    
+    for idx, i in enumerate(meal_names):
+        if flt_text.lower() in i.lower():
+            st.button(str(i) , on_click = lambda: show_reciepe(idx)  )
+
+with col2:
+    st.text_input(label = "filter reciepe details", value  = "",  key = "search2" )
+    txt = st.text_area(
+                        label = "Recipe Details",
+                        key = "txtarea",
+                        value = "",
+                        height = 700,
+                        )
+
+    
---
+++ yaml_digest.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 21:24:59 2024
+
+@author: amrut
+"""
+
+import yaml, os
+
+cwd = os. path. dirname(__file__)
+food_folder =  cwd + '/food'
+err_flag = not os.path.exists( food_folder )
+
+if err_flag:
+    raise SystemExit()
+
+meal_files = os.listdir( food_folder )
+
+meal_info = []
+
+for file in meal_files:
+    with open(food_folder+ '/'+ file, 'r') as file:
+        meal_info_each = yaml.safe_load(file)
+        meal_info .append ( meal_info_each )
+
